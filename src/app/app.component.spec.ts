0ff358863687c8a16ef6d035183756116aaf24e2
HenryAprendo <hjsmipuc@gmail.com>
2023-05-24 13:17:45 -0500

    Eliminando el fixture de DebugElement

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,9 @@
 import { TestBed, ComponentFixture} from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { DebugElement } from '@angular/core';
 
 describe('Testing for component root', () => {
 
   let component: ComponentFixture<AppComponent>;
-  let fixture: DebugElement;
 
   beforeEach(async () => {
      await TestBed.configureTestingModule({
