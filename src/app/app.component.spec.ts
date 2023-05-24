b9ec4c9588ad15af998a5e70b6bf5b75331e0ade
HenryAprendo <hjsmipuc@gmail.com>
2023-05-24 12:56:24 -0500

    Configuración del modulo de pruebas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,19 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, ComponentFixture} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { DebugElement } from '@angular/core';
 
-describe('AppComponent', () => {
+describe('Testing for component root', () => {
+
+  let component: ComponentFixture<AppComponent>;
+  let fixture: DebugElement;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+     await TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    })
+    .compileComponents();
+
+
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'practice-two'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('practice-two');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('practice-two app is running!');
-  });
 });
