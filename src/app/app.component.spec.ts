74cd2f260f8e7ff4295dec33b69eef125c0c1821
HenryAprendo <hjsmipuc@gmail.com>
2023-05-24 13:13:37 -0500

    Configuración del modulo de pruebas de routing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed, ComponentFixture} from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { DebugElement } from '@angular/core';
 
-describe('Testing for component root', () => {
+describe('Testing for component main', () => {
 
   let component: ComponentFixture<AppComponent>;
   let fixture: DebugElement;
 
   beforeEach(async () => {
      await TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     })
     .compileComponents();
 
